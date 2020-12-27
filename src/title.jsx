e9c69ba382574cf1afe5b7b99bcb2d4f5f3fe6a5
rakshaa2000 <rakshaa2000@gmail.com>
2020-12-27 18:13:11 +0530

    changes

--- src/title.jsx
+++ src/title.jsx
@@ -12,7 +12,7 @@ const Title = () => {
             <h1 className="typewriting" style={{textAlign:"center",  wordWrap:"break-word"}}>Christmas for Everyone- Joy of Giving <CardGiftcardIcon fontSize="large"></CardGiftcardIcon></h1>
             <h1 className="caption" style={{fontFamily:"'Satisfy', cursive"}}>Lifting up with hands of help</h1>
             </div>
-            <img className="img-right" src={santa} alt="" height="200px" width="200px"/>
+            <a href="https://repl.it/@rakshaa2000/Box-it#main.py" target="_blank" rel="noreferrer"><img className="img-right" src={santa} alt="" height="200px" width="200px"/></a>
         </div>
     )
 };
