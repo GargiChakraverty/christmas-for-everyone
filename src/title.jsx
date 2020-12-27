865f59422869344b72e090bf3e007c0909ce7953
rakshaa2000 <rakshaa2000@gmail.com>
2020-12-27 18:03:44 +0530

    changes

--- src/title.jsx
+++ src/title.jsx
@@ -7,7 +7,7 @@ import CardGiftcardIcon from '@material-ui/icons/CardGiftcard';
 const Title = () => {
     return(
         <div className="div" style={{display: "flex", width:"100%", margin:"auto", justifyContent:"space-evenly"}}>
-            <a href="https://repl.it/@rakshaa2000/Christmas-Hangman#main.py" target="_blank"><img className="img-left" src={tree} alt="" height="200px" width="200px"/></a>
+            <a href="https://repl.it/@rakshaa2000/Christmas-Hangman#main.py" target="_blank" rel="noreferrer"><img className="img-left" src={tree} alt="" height="200px" width="200px"/></a>
             <div style={{margin:"10px", alignItems:"center"}}>
             <h1 className="typewriting" style={{textAlign:"center",  wordWrap:"break-word"}}>Christmas for Everyone- Joy of Giving <CardGiftcardIcon fontSize="large"></CardGiftcardIcon></h1>
             <h1 className="caption" style={{fontFamily:"'Satisfy', cursive"}}>Lifting up with hands of help</h1>
